2d5930f23d4528c9f7125850c53f6af5814cf5d5
Ethan Swan <eswan18@rocketmail.com>
2022-02-07 20:07:35 -0600

    Update environments to support enclosing.

--- src/_environment.py
+++ src/_environment.py
@@ -1,23 +1,35 @@
+from __future__ import annotations
+
 from ._token import Token
 from ._errors import LoxRuntimeError
 
 class Environment:
 
-    def __init__(self):
+    def __init__(self, enclosing: Environment = None):
         self.values: dict[str, object] = {}
+        self.enclosing = enclosing
 
     def define(self, name: str, value: object) -> None:
         self.values[name] = value
 
     def get(self, token: Token) -> object:
-        if token.lexeme in self.values:
+        name = token.lexeme
+        if name in self.values:
+            # If the variable is in this environment, return its value.
             return self.values[token.lexeme]
+        elif self.enclosing is not None:
+            # If this environment is enclosed by another, check that one (and its parents).
+            return self.enclosing.get(token)
         else:
-            raise LoxRuntimeError(token, f"Undefined variable '{token.lexeme}'.")
+            raise LoxRuntimeError(token, f"Undefined variable '{name}'.")
     
     def assign(self, token: Token, value: object) -> None:
         name = token.lexeme
         if name in self.values:
+            # If the variable is in this environment, update it here.
             self.values[name] = value
+        elif self.enclosing is not None:
+            # If this environment is enclosed by another, check that one (and its parents).
+            self.enclosing.assign(token, value)
         else:
             raise LoxRuntimeError(token, "Undefined variable '{name}'.")
